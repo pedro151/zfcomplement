65b87e65b6de35c4c843a79f287f7bf78e60c01d
pedro <phacl@hotmail.com>
2015-01-12 10:47:46 -0200

    comit

--- library/Javascript/JqueryValidate/Rule/EmailAddress.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace StrokerForm\Renderer\JqueryValidate\Rule;
-
-class EmailAddress extends AbstractRule
-{
-    protected $_rule = 'email';
-    protected $_message = 'Email address is invalid';
-}
